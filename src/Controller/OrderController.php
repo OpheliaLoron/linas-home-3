27733be727ff6b9c226fc4b4349253992eab8773
OpheliaLoron <120174913+OpheliaLoron@users.noreply.github.com>
2023-07-02 17:46:43 +0200

    Avancée paiement

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -7,6 +7,9 @@ use App\Entity\OrderDetails;
 use App\Entity\Orders;
 use App\Form\OrderType;
 use Doctrine\ORM\EntityManagerInterface;
+use Stripe\Checkout\Session;
+use Stripe\PaymentMethod;
+use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -94,8 +97,31 @@ class OrderController extends AbstractController
                 // dd($product);
             }
 
-            $this->entityManager->flush();
+            // $this->entityManager->flush();
 
+           Stripe::setApiKey($stripeSecretKey);
+           
+           $YOUR_DOMAIN = 'http://127.0.0.1:8000';
+
+           Session::create();
+           
+           $checkout_session = Session::create([
+            // 'payment_method_types' => ['card'],
+             'line_items' => [[
+               # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+               'price' => '{{PRICE_ID}}',
+               'quantity' => 1,
+             ]],
+             'mode' => 'payment',
+             'success_url' => $YOUR_DOMAIN . '/success.html',
+             'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+           ]);
+
+           dump($checkout_session->url);
+           dd($checkout_session);
+           
+           header("HTTP/1.1 303 See Other");
+           header("Location: " . $checkout_session->url);
             return $this->render('order/add.html.twig', [
                 'cart' => $cart->getFull(),
                 'carrier' => $carriers,
